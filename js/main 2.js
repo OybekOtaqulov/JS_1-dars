3cfe48eba5f416b208ffdc2cdbcea6179c8f99e5
Ogurchik0909 <ogurchik082007@gmail.com>
2023-05-20 12:30:59 +0500

    JS 1-dars

---
+++ js/main 2.js
@@ -0,0 +1,27 @@
+if (45 == 45){
+    console.log("Bu sonlar teng!")
+}
+else if (45 == 45){
+    console.log("Endi bu son 45ga teng!")
+}
+else {
+    console.log("Ikkala son ham 45ga teng emas!")
+}
+if (45 == 45){
+    console.log("Bu son 45ga teng!")
+}
+else if (45 == 45){
+    console.log("Endi bu son 45ga teng!")
+}
+else {
+    console.log("Ikkala son ham 45ga teng emas!")
+}
+if (45 == 45){
+    console.log("Bu son 45ga teng!")
+}
+else if (45 == 45){
+    console.log("Endi bu son 45ga teng!")
+}
+else {
+    console.log("Ikkala son ham 45ga teng emas!")
+}
---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*console.log("Men faqat console oynasida ko'rinaman!");*/
+/*alert("Saytimizga xush kelibsiz!");*/
+/*document.write("Men bodiyda ko'rinaman");*/
+/*let x=2;*/
+/*let y=5*/
+/*x=10*/
+/*let x=2,y=5,z=10*/
+/*let color = "f1f1f1"*/
+/*let salom = "Assalomu Alaykum!";
+document.write(salom);*/
+/* data types: number,string */
+/*let x=5;
+let soz = "JavaScript"*/
+/*document.write(typeof soz)*/
+/* JS arifmetik hisoblashlar */
+let x=5;
+/*x+=5;*/
+/*x-=2;*/
+/*x*=5;*/
+/*x/=2;*/
+/*let qoldiq = x%4;
+let daraja = x**5;*/
+/*+++x;*/
+/*--x;*/
+/*x++;*/
+/*x--;*/
+/*document.write(x)*/
+/* if = если */
+/* else = тогда */
+/*if ("Salom" == "Salom"){
+    console.log("Ular teng!");
+}
+else{
+    console.log("Ular teng emas!")
+}*/
+
+/* if = если */
+/* else if = может быть */
+/* else = тогда */
+
+if (45 == 45){
+    console.log("Bu son 45ga teng!")
+}
+else if (45 == 45){
+    console.log("Endi bu son 45ga teng!")
+}
+else {
+    console.log("Ikkala son ham 45ga teng emas!")
+}
+
+
+
+
+
+
